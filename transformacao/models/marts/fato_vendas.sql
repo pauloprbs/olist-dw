2c4f388afc9210a3ffcf9ff929b959c7434b9328
pauloprbs <paulorenato_bs@live.com>
2025-12-03 17:15:38 -0300

    feat: inclusão das datas de venda na tabela fato.

--- transformacao/models/marts/fato_vendas.sql
+++ transformacao/models/marts/fato_vendas.sql
@@ -13,10 +13,15 @@ SELECT
     itens.product_id AS id_produto,
     itens.seller_id AS id_vendedor,
     pedidos.customer_id AS id_cliente_pedido,
+    
     TO_CHAR(pedidos.order_purchase_timestamp::DATE, 'YYYYMMDD')::INTEGER AS id_data_compra,
+    
+    pedidos.order_purchase_timestamp::DATE AS data_pedido, 
+
     itens.price AS valor_item,
     itens.freight_value AS valor_frete,
     (itens.price + itens.freight_value) AS valor_total,
+    
     pedidos.order_status AS status_pedido
 FROM itens
 JOIN pedidos ON itens.order_id = pedidos.order_id
